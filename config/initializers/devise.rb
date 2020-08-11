c5caca316f57c509da8735d454ec6f2f68b378b2
afsanehs <afsaneh.samettehrani@essec.edu>
2020-08-11 23:56:19 +0200

    email sender

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -24,7 +24,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'sandboxbcc6fb78478347aca3a3a591b291f4d6.mailgun.org'
+  config.mailer_sender = 'afsaneh.samettehrani@essec.edu'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
